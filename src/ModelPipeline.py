d8092a19a2fa6a940a774b5506d46c4ec80c66f6
M007 <mjallouli007@gmail.com>
2020-11-29 05:22:38 +0100

    first commit

---
+++ src/ModelPipeline.py
@@ -0,0 +1,10 @@
+import typing
+class ModelPipeline:
+    def __init__(self):
+        pass
+    def transform(self):
+        pass
+    def fit(self):
+        pass
+    def eval(self,metric:List[str]):
+
