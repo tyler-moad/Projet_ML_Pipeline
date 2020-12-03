1fcfbe5bd3db760cf57ddd3cb85e6df1ad26d444
REDABENSAID <reda@DESKTOP-C9V7013.localdomain>
2020-12-03 23:03:37 +0100

    train test split and cross val

--- src/model/ModelPipeline.py
+++ src/model/ModelPipeline.py
@@ -27,4 +27,39 @@ class ModelPipeline:
         
     def cross_val(X,y,scoring:str):
 
+    def train_test_split(X,Y,test_size = 0.1, validation_size = 0):
+    
+        if len(X) != len(Y) : 
+            print("error")
+        permutation = np.random.permutation(len(X))
+        X = X[permutation]
+        Y = Y[permutation]
+        if test_size + validation_size >= 1 : 
+            print("error")
+        else : 
+            X_train = X[:int((1-test_size-validation_size)*len(X))]
+            Y_train = Y[:int((1-test_size-validation_size)*len(Y))]
+            X_validation = X[int((1-test_size-validation_size)*len(X)):int((1-test_size)*len(X))]
+            Y_validation = Y[int((1-test_size-validation_size)*len(Y)):int((1-test_size)*len(Y))]
+            X_test = X[int((1-test_size)*len(X)):]
+            Y_test = Y[int((1-test_size)*len(Y)):]
+
+
+        return X_train, Y_train, X_validation, Y_validation, X_test, Y_test
+
+
+    def cross_validation (X_train,Y_train, model = "dip_leurning", k = 10) : 
+    
+        evaluation = []
+        if len(X) != len(Y) : 
+            print("error")
+        for i in range(k):
+            X_test_fold = X_train[int((i/k)*len(X)):int(((i+1)/k)*len(X))]
+            Y_test_fold = Y_train[int((i/k)*len(Y)):int(((i+1)/k)*len(Y))]
+            X_train_fold = np.concatenate((X_train[:int((i/k)*len(X))],X_train[int(((i+1)/k)*len(X)):]))
+            Y_train_fold = np.concatenate((Y_train[:int((i/k)*len(Y))],Y_train[int(((i+1)/k)*len(Y)):]))
+            train(X_train_fold,Y_train_fold, model)
+            evaluation.append(test(X_test_fold,Y_test_fold, model))
+        return evaluation
+
 
