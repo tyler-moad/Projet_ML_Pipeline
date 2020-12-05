7487f83e395467083f0559e5e3681de4f4a9d1f6
MarouaneMaachou <marouane.maachou@imt-atlantique.net>
2020-12-05 18:12:44 +0100

    Adding spelling error correction and full preprocessing

--- src/preprocessing/DataLoader.py
+++ src/preprocessing/DataLoader.py
@@ -2,10 +2,27 @@ import numpy as np
 import pandas as pd
 
 
+def LD(s, t):
+    if s == "":
+        return len(t)
+    if t == "":
+        return len(s)
+    if s[-1] == t[-1]:
+        cost = 0
+    else:
+        cost = 1
+
+    res = min([LD(s[:-1], t) + 1,
+               LD(s, t[:-1]) + 1,
+               LD(s[:-1], t[:-1]) + cost])
+
+    return res
+
+
 class DataLoader:
 
     def __init__(self, data_path, missing_data_strategy: str = "mean", categorical_encoding: str = "label",
-                 test_size: float = 0.1, validation_size=0.1, categorical_threshold=15):
+                 test_size: float = 0.1, validation_size=0.1, categorical_threshold=15, headers: bool = True):
 
         if missing_data_strategy in ["mean", "median", "radical"]:
             self.missing_stategy = missing_data_strategy
@@ -19,20 +36,21 @@ class DataLoader:
 
         self.test_size = test_size
         self.validation_size = validation_size
-        self.data = pd.read_csv(data_path)
+        if headers:
+            self.data = pd.read_csv(data_path)
+        else:
+            self.data = pd.read_csv(data_path, header=None)
         self.categorical_threshold = categorical_threshold
 
     def find_continuous_columns(self):
         for col_name in self.data.columns:
             if self.data[col_name].dtype == 'O':
-                print(col_name)
                 if len(set(self.data[col_name])) > self.categorical_threshold:
                     # self.data[col_name].to_numeric(convert_numeric=True)
                     self.data[col_name] = pd.to_numeric(self.data[col_name], downcast='float', errors='coerce')
 
     def correct_typos(self):
         for col_name in self.data.columns:
-            print(col_name)
             if self.data[col_name].dtype == 'O':
                 values_taken = list(set(self.data[col_name]))
                 distance_matrix = [[LD(values_taken[i], values_taken[j]) for i in range(len(values_taken))] for j in
@@ -41,7 +59,6 @@ class DataLoader:
                 typos = [point for point in zip(typos[0], typos[1]) if point[0] < point[1]]
                 for typo in typos:
                     values_to_exchange = values_taken[typo[0]], values_taken[typo[1]]
-                    print(values_to_exchange)
                     self.data[col_name][self.data[col_name] == values_to_exchange[0]] = values_to_exchange[1]
 
     def encode_categorical_columns(self):
@@ -89,7 +106,6 @@ class DataLoader:
     def infer_missing_data(self):
         incomplete_columns = self.find_incomplete_columns()
         for col_name in incomplete_columns:
-            print(col_name)
             self.complete_column(col_name=col_name, missing_strategy=self.missing_stategy)
 
     def transform(self):
@@ -97,4 +113,3 @@ class DataLoader:
         self.infer_missing_data()
         self.correct_typos()
         self.encode_categorical_columns()
-
