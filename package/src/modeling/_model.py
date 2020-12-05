ed25a06e500155160c36032f2c4698f4b49db4f0
MouadJallouli <mjallouli007@gmail.com>
2020-12-05 01:21:23 +0100

    add f1_score_support method

--- package/src/modeling/_model.py
+++ package/src/modeling/_model.py
@@ -1,5 +1,6 @@
 from itertools import product
 import numpy as np
+import pandas as pd
 
 class Model():
 
@@ -30,7 +31,7 @@ class Model():
         return np.mean(evaluation)
 
     """@Author Moad Taoufik"""
-    def generateGrid(self,params):
+    def _generateGrid(self,params):
         keys, values = zip(*params.items())
         grid = []
         for v in product(*values):
@@ -45,7 +46,7 @@ class Model():
           The best found parameters and the best score are saved as a class attributes.
                
         """
-        params_grid = self.generateGrid(params)
+        params_grid = self._generateGrid(params)
         best_param = None
         best_score = 0
         for param in params_grid:
@@ -60,27 +61,6 @@ class Model():
         self.best_params_ = best_param
 
     """@Author Mouad Jallouli"""
-    @staticmethod
-    def f1_score(y_true, y_predict):
-        p = 0
-        r = 0
-        f = 0
-        n_classes = y_true.nunique()
-        for i in range(n_classes):
-            tp = sum(((y_true == i) & (y_predict == i)))
-            fp = sum(((y_true != i) & (y_predict == i)))
-            fn = sum(((y_true == i) & (y_predict != i)))
-            if fp == 0: # edge case
-                p = 1
-            else:
-                p = tp / (tp + fp)
-            if fn == 0: # edge case
-                r = 1
-            else:
-                r = tp / (tp + fn)
-            f += 2 * p * r / (p + r)
-    
-        return f/n_classes 
     @staticmethod
     def _f1_score(y_true, y_predict):
         p = 0
@@ -102,9 +82,43 @@ class Model():
             f += 2 * p * r / (p + r)
     
         return f/n_classes 
+
+    """@Author Mouad Jallouli"""
+    @staticmethod
+    def f1_score_support(y_true, y_predict):
+        """ 
+           Returns pandas dataframe with precision, recall and f1 score for each class
+          :params:
+                y_true: array-like, true target
+                y_predict: array-like, predicted target
+          
+        """
+        p = 0
+        r = 0
+        f = 0
+        D = {"Precision":{},"Recall":{},"F1_score":{}}
+        n_classes = y_true.nunique()
+        for i in range(n_classes):
+            tp = sum(((y_true == i) & (y_predict == i)))
+            fp = sum(((y_true != i) & (y_predict == i)))
+            fn = sum(((y_true == i) & (y_predict != i)))
+            if fp == 0: # edge case
+                p = 1
+            else:
+                p = tp / (tp + fp)
+            if fn == 0: # edge case
+                r = 1
+            else:
+                r = tp / (tp + fn)
+            D["Precision"]["class "+str(i)] = p
+            D["Recall"]["class "+str(i)] = r
+            D["F1_score"]["class "+str(i)] = 2 * p * r / (p + r)
+            
+    
+        return pd.DataFrame(D)
     
     """@Author Moad Taoufik"""
     @staticmethod    
-    def accuracy (y_true, y_predict):
+    def _accuracy (y_true, y_predict):
         good_predictions = np.sum((y_true == y_predict)*1)
         return good_predictions /len(y_true)
