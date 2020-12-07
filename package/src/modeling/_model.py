97e3b19023b6ad78bb8c8485fcf921a808939204
REDABENSAID <reda@DESKTOP-C9V7013.localdomain>
2020-12-07 02:22:15 +0100

    working class model

--- package/src/modeling/_model.py
+++ package/src/modeling/_model.py
@@ -1,29 +1,35 @@
 from itertools import product
-class Model()
-    def __init__(self, model):
+class Model():
+    def __init__(self, model, metric:str = "accuracy"):
         self.model = model
+        self.metric = metric
         self.best_score = None
         self.best_params = None
 
-    def fit(self,X,Y,params_dic):
-        self.model.set_params(**params_dic)
-        self.model.fit(X,Y)
-    def predict(self,X):
-        return self.model.predict(X)
+    #def fit(self,X,Y,params_dic):
+     #   self.model.set_params(**params_dic)
+      #  self.model.fit(X,Y)
+    #def predict(self,X):
+     #   return self.model.predict(X)
 
 
-    def cross_validation_score(self, X_train: np.array, y_train: np.array, k=10):
+    def cross_validation_score(self, X_train: np.array, y_train: np.array,  k=10):
         evaluation = []
+        
         for i in range(k):
             X_test_fold = X_train[int((i / k) * len(X_train)):int(((i + 1) / k) * len(X_train))]
             y_test_fold = y_train[int((i / k) * len(y_train)):int(((i + 1) / k) * len(y_train))]
+            
             X_train_fold = np.concatenate(
                 (X_train[:int((i / k) * len(X_train))], X_train[int(((i + 1) / k) * len(X_train)):]))
             y_train_fold = np.concatenate(
                 (y_train[:int((i / k) * len(y_train))], y_train[int(((i + 1) / k) * len(y_train)):]))
             self.model.fit(X_train_fold, y_train_fold)
             y_pred = self.model.predict(X_test_fold)
-            evaluation.append(f1_score(y_test_fold, y_pred))
+            if self.metric == "accuracy" : 
+                evaluation.append(self.accuracy(y_test_fold, y_pred))
+            else : 
+                evaluation.append(self.f1_score(y_test_fold, y_pred))
         return np.mean(evaluation)
 
     def generateGrid(self,params):
@@ -32,31 +38,45 @@ class Model()
         for v in product(*values):
             d = dict(zip(keys, v))
             grid.append(d)
+        return grid
+        
 
     def gridsearchCV(self,X,y,params):
-        param_grid = self.generateGrid(params)
+        params_grid = self.generateGrid(params)
         scores_dict = {}
         best_param = None
         best_score = 0
         for param in params_grid:
+            print(param)
             self.model.set_params(**param)
             score = self.cross_validation_score(X,y)
-            scores_dict[param] = score
+            print(score)
             if score > best_score:
                 best_score = score
                 best_param = param
         self.best_score = best_score
         self.best_params = best_param
-
+    
+    @staticmethod
+    #FIXME
+    # f1_score fiha mouchkil
     def f1_score(y_true, y_predict):
+        from sklearn.metrics import f1_score
         p = 0
         r = 0
+        f = 0
         n_classes = y_true.nunique()
-        print(n_classes)
         for i in range(n_classes):
             tp = sum(((y_true == i) & (y_predict == i)))
             fp = sum(((y_true != i) & (y_predict == i)))
             fn = sum(((y_true == i) & (y_predict != i)))
-            p += tp / (tp + fp)
-            r += tp / (tp + fn)
-        return 2 * p * r / (p + r)
+            p = tp / (tp + fp)
+            r = tp / (tp + fn)
+            f += 2 * p * r / (p + r)
+    
+        return f/n_classes#f1_score(y_true, y_predict)
+
+    @staticmethod    
+    def accuracy (y_true, y_predict):
+        good_predictions = np.sum((y_true == y_predict)*1)
+        return good_predictions /len(y_true)
